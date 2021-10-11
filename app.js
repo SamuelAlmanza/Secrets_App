f846620f7b63601b2d4b59615d358573e66828d3
SamuelAlmanza <77166936+SamuelAlmanza@users.noreply.github.com>
2021-10-11 14:18:01 -0500

    updated port

--- app.js
+++ app.js
@@ -168,7 +168,7 @@ app.post("/login", function(req, res){
 let port = process.env.PORT;
 if (port == null || port == ""){
     port = 3000;
-}
+};
 
 app.listen(port, function(){
     console.log("Server has started successfully.")
